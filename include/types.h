d9efc66dd03b64b2c1e9ce113508b8066fa101c8
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-06-01 12:41:40 +0200

    feat: update types with pointers to other types

--- include/types.h
+++ include/types.h
@@ -1,6 +1,8 @@
 #ifndef TYPES_H
 #define TYPES_H
 
+#include <stdbool.h>
+
 #define ML_CLASS_NAME 20
 #define L_CLASS_YEAR 5
 #define ML_STUDENT_NAME 50
@@ -8,32 +10,38 @@
 #define ML_TEST_DESCRIPTION 100
 #define L_TEST_DATE 10
 
-typedef struct class_type {
-  int id;
-  char name[ML_CLASS_NAME+1];
-  char year[L_CLASS_YEAR+1];
-} *Class;
+typedef unsigned int Id;
 
-typedef struct student_type {
-  int id;
-  char first_name[ML_STUDENT_NAME+1];
-  char last_name[ML_STUDENT_NAME+1];
-  int class_id;
-} *Student;
+typedef struct grade_type {
+  int score;
+  Id student_id;
+  Id test_id;
+} *Grade;
 
 typedef struct test_type {
-  int id;
+  Id id;
   char name[ML_TEST_NAME+1];
   char description[ML_TEST_DESCRIPTION+1];
   int max_score;
   char date[L_TEST_DATE+1];
-  int class_id;
+  Id class_id;
+  Grade *grades;
 } *Test;
 
-typedef struct grade_type {
-  int score;
-  int student_id;
-  int test_id;
-} *Grade;
+typedef struct student_type {
+  Id id;
+  char first_name[ML_STUDENT_NAME+1];
+  char last_name[ML_STUDENT_NAME+1];
+  Id class_id;
+  Grade *grades;
+} *Student;
+
+typedef struct class_type {
+  Id id;
+  char name[ML_CLASS_NAME+1];
+  char year[L_CLASS_YEAR+1];
+  Student *students;
+  Test *tests;
+} *Class;
 
 #endif
