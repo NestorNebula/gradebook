ad5e585cd81f1dbd79e4484fd6f0b8a293ee3ba6
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-06-04 09:23:26 +0200

    feat: add input module's header

---
+++ include/input.h
@@ -0,0 +1,28 @@
+#ifndef INPUT_H
+#define INPUT_H
+
+#include <stdbool.h>
+
+/*
+ * Input:
+ * Every function in this module clears what follows the input
+ * up to and including the new line character or EOF.
+ */
+
+/*
+ * read_number:
+ * Reads a number from stream.
+ * If a number could be read, returns it and stores true in success,
+ * else, returns 0 and stores false in success.
+ */
+int read_number(bool *success, FILE *stream);
+
+/*
+ * Reads a string from stream and stores it in output, 
+ * followed by a null character.
+ * Reads a maximum of max_len - 1 characters.
+ * Returns the number of characters read.
+ */
+int read_string(char *output, int max_len, FILE *stream);
+
+#endif
