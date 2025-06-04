d732c12664848206949ffe8cb10e06e9d0a6d677
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-06-04 09:52:36 +0200

    feat: add input module's functions

---
+++ src/input.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include "input.h"
+
+/*
+ * clear_line:
+ * Reads stream until meaning a new line character or EOF.
+ */
+static void clear_line(FILE *stream);
+
+int read_number(bool *success, FILE *stream) {
+  int num = 0;
+  *success = fscanf(stream, "%d", &num);
+  clear_line(stream);
+  return num;
+}
+
+int read_string(char *output, int max_len, FILE *stream) {
+  int ch, i = 0;
+  while (i < max_len - 1 && (ch = getc(stream)) != '\n' && ch != EOF) {
+    output[i++] = ch;
+  }
+  output[i] = '\0';
+  if (i == max_len - 1) clear_line(stream);
+  return i;
+}
+
+static void clear_line(FILE *stream) {
+  int ch = getc(stream);
+  while (ch != '\n' && ch != EOF) ch = getc(stream);
+}
