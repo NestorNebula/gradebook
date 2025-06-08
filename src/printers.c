590166361dc37769c0374a2ff16a1381d9e30c3a
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-06-08 12:02:32 +0200

    feat: add print_dividing_line function

--- src/printers.c
+++ src/printers.c
@@ -3,27 +3,35 @@
 #include "printers.h"
 
 #define L_TEST_COL 10
-
 #define L_NAME_COL (ML_STUDENT_NAME / 2 + 1)
+#define ML_NAME ((L_NAME_COL - 1) / 2)
+
+/*
+ * print_dividing_line: 
+ * Prints a dividing line. Uses separator at the beginning and end of line
+ * as well as between tests.
+ */
+static void print_dividing_line(Test_p *tests, char separator);
 
 void print_table_header(const char *title, Test_p *tests) {
-  printf("%*s |", L_NAME_COL - 1, title);
+  print_dividing_line(tests, '-');
+  printf("|%*s |", L_NAME_COL - 1, title);
   for (Test_p *p = tests; *p != NULL; p++) {
     printf("%*s|", L_TEST_COL, (*p)->name);
   }
   printf("\n");
+  print_dividing_line(tests, '|');
 }
 
 void print_student_name(Student_p student) {
-  int max_name_len = (L_NAME_COL - 1) / 2;
-  printf("%.*s %.*s", 
-         max_name_len, student->last_name,
-         max_name_len, student->first_name);
+  printf("|%.*s %.*s", 
+         ML_NAME, student->last_name,
+         ML_NAME, student->first_name);
   int first_len = strlen(student->first_name),
     last_len = strlen(student->last_name);
   int remaining_len = L_NAME_COL
-    - (first_len > max_name_len ? max_name_len : first_len)
-    - (last_len > max_name_len ? max_name_len : last_len) 
+    - (first_len > ML_NAME ? ML_NAME : first_len)
+    - (last_len > ML_NAME ? ML_NAME : last_len) 
     - 1;
   while (remaining_len-- > 0) printf(" ");
   printf("|");
@@ -50,9 +58,25 @@ void print_tests_grades(Test_p* tests, Grade_p *grades) {
 void print_table_footer(Test_p *tests) {
   int test_count = 0;
   for (Test_p *p = tests; *p != NULL; p++, test_count++);
-  printf("%*s|", L_NAME_COL, "Max Score");
+  print_dividing_line(tests, '|');
+  printf("|%*s|", L_NAME_COL, "Max Score");
   for (int i = 0; i < test_count; i++) {
     printf("%*d|", L_TEST_COL, tests[i]->max_score);
+  }
+  printf("\n");
+  print_dividing_line(tests, '-');
+  printf("\n");
+}
+
+static void print_dividing_line(Test_p *tests, char separator) {
+  putchar(separator);
+  for (int i = 0; i < L_NAME_COL; i++) printf("-");
+  putchar(separator);
+  for (Test_p *p = tests; *p != NULL; p++) {
+    for (int i = 0; i < L_TEST_COL; i++) {
+      printf("-");
+    }
+    putchar(separator);
   }
   printf("\n");
 }
