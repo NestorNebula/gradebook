c9c888f02e6c31e788ac690e0d04966f86935d27
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-06-08 10:18:56 +0200

    test: update function fakes after db types changes

--- tests/main.cpp
+++ tests/main.cpp
@@ -15,7 +15,7 @@ Student_p student_t = &student_test;
 struct test_type test_test = { 1, "Test test", "", 100, "2024-10-01", 1, NULL };
 Test_p test_t = &test_test;
 
-int score = 0;
+float score = 0;
 struct grade_type grade_test = { &score, 1, 1 };
 Grade_p grade_t = &grade_test;
 
@@ -25,6 +25,7 @@ FAKE_VALUE_FUNC(int, sqlite3_exec, sqlite3 *, const char *,
 FAKE_VALUE_FUNC(int, sqlite3_prepare_v2, sqlite3 *, const char *,
   int, sqlite3_stmt **, const char **);
 FAKE_VALUE_FUNC(int, sqlite3_column_int, sqlite3_stmt *, int);
+FAKE_VALUE_FUNC(double, sqlite3_column_double, sqlite3_stmt *, int);
 FAKE_VALUE_FUNC(const unsigned char *, sqlite3_column_text, 
   sqlite3_stmt *, int);
 FAKE_VALUE_FUNC(int, sqlite3_column_count, sqlite3_stmt *);
