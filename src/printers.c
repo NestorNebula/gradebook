a35b4a7f0c02238e69e58fe6c8e16c961aca3692
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-06-08 11:11:55 +0200

    feat: update printers helpers functions
    Update informations printed
    Update macro names and definitions for length values

--- src/printers.c
+++ src/printers.c
@@ -2,24 +2,26 @@
 #include <string.h>
 #include "printers.h"
 
-int name_column_length = ML_STUDENT_NAME + 1;
+#define L_TEST_COL 10
+
+#define L_NAME_COL (ML_STUDENT_NAME / 2 + 1)
 
 void print_table_header(const char *title, Test_p *tests) {
-  printf("%*s|", name_column_length, title);
+  printf("%*s |", L_NAME_COL - 1, title);
   for (Test_p *p = tests; *p != NULL; p++) {
-    printf("%*s|", L_TEST_DATE, (*p)->date);
+    printf("%*s|", L_TEST_COL, (*p)->name);
   }
   printf("\n");
 }
 
 void print_student_name(Student_p student) {
-  int max_name_len = ML_STUDENT_NAME / 2;
-  printf("\n%.*s %.*s", 
+  int max_name_len = (L_NAME_COL - 1) / 2;
+  printf("%.*s %.*s", 
          max_name_len, student->last_name,
          max_name_len, student->first_name);
   int first_len = strlen(student->first_name),
     last_len = strlen(student->last_name);
-  int remaining_len = name_column_length
+  int remaining_len = L_NAME_COL
     - (first_len > max_name_len ? max_name_len : first_len)
     - (last_len > max_name_len ? max_name_len : last_len) 
     - 1;
@@ -32,15 +34,15 @@ void print_tests_grades(Test_p* tests, Grade_p *grades) {
   for (Test_p *p = tests; *p != NULL; p++, test_count++);
   for (int i = 0; i < test_count; i++) {
     if (grades[grade_index] == NULL) {
-      printf("%*s|", L_TEST_DATE, "/");
+      printf("%*s|", L_TEST_COL, "/");
     } else if 
       (tests[i]->id != grades[grade_index]->test_id) {
-      printf("%*s|", L_TEST_DATE, "/");
+      printf("%*s|", L_TEST_COL, "/");
     } else if (grades[grade_index]->score == NULL) {
-      printf("%*s|", L_TEST_DATE, "/");
+      printf("%*s|", L_TEST_COL, "/");
       grade_index++;
     } else {
-      printf("%*g|", L_TEST_DATE, *grades[grade_index++]->score);
+      printf("%*g|", L_TEST_COL, *grades[grade_index++]->score);
     }
   }
 }
@@ -48,9 +50,9 @@ void print_tests_grades(Test_p* tests, Grade_p *grades) {
 void print_table_footer(Test_p *tests) {
   int test_count = 0;
   for (Test_p *p = tests; *p != NULL; p++, test_count++);
-  printf("%*s|", name_column_length, "Max Score");
+  printf("%*s|", L_NAME_COL, "Max Score");
   for (int i = 0; i < test_count; i++) {
-    printf("%*d|", L_TEST_DATE, tests[i]->max_score);
+    printf("%*d|", L_TEST_COL, tests[i]->max_score);
   }
   printf("\n");
 }
